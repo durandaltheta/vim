83ad9064ea1dc9636dca278ce3848d8184a14e65
durandaltheta <blaynetdennis@gmail.com>
2016-11-09 19:37:02 +0000

    replaced ~ with /home/

--- vimfiles/install.sh
+++ vimfiles/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 
-cp -rf .vim ~/ 
-cp -f .vimrc ~/ 
-
 username=`whoami`
 
-chown -R $username:$username ~/.vim 
-chown $username:$username ~/.vimrc
+cp -rf .vim /home/$username/ 
+cp -f .vimrc /home/$username/ 
+
+
+chown -R $username:$username /home/$username/.vim 
+chown $username:$username /home/$username/.vimrc
 
 echo "done installing cusom vim files"

4f2feaf2b0dba3b3e3302b3e823ebd313be316a5
durandaltheta <blaynetdennis@gmail.com>
2017-02-22 14:24:16 -0500

    realized I could install cscope via apt

--- install_vim.sh
+++ install_vim.sh
@@ -15,26 +15,28 @@ cp -f .vimrc $HOME/
 
 # copy cscope_gen script to local executables directory
 sudo -E cp $SCRIPT_DIR/cscope_gen /usr/local/bin 
+sudo chmod +x /usr/local/bin/cscope_gen
+#sudo chown $username:$username /usr/local/bin/cscope_gen
 
 # compile and install cscope 
-CUR_DIR=`pwd`
-sudo chown -R $username:$username $SCRIPT_DIR/cscope-15.8b
-cd $SCRIPT_DIR/cscope-15.8b 
-#touch aclocal.m4
-#touch Makefile.am
-#touch configure.ac 
-aclocal 
-automake  --add-missing
-autoconf
-#sudo ln -s /usr/bin/automake-1.15 /usr/bin/automake-1.14
-#sudo ln -s /usr/bin/aclocal-1.15 /usr/bin/aclocal-1.14
-chmod +x ./configure
-chmod +x ./install-sh
-pwd
-sudo ./configure
-make 
-sudo make install
-cd $CUR_DIR
+#CUR_DIR=`pwd`
+#sudo chown -R $username:$username $SCRIPT_DIR/cscope-15.8b
+#cd $SCRIPT_DIR/cscope-15.8b 
+##touch aclocal.m4
+##touch Makefile.am
+##touch configure.ac 
+#aclocal 
+#automake  --add-missing
+#autoconf
+##sudo ln -s /usr/bin/automake-1.15 /usr/bin/automake-1.14
+##sudo ln -s /usr/bin/aclocal-1.15 /usr/bin/aclocal-1.14
+#chmod +x ./configure
+#chmod +x ./install-sh
+#pwd
+#sudo ./configure
+#make 
+#sudo make install
+#cd $CUR_DIR
 
 
 # make sure the files are owned by the correct person
